a0ba7924b22f407eb7deaf8a0437017c5f29c482
James Bowmnan <jamesb@excamera.com>
2013-06-22 23:15:36 -0700

    Unit tests, minimal load0

--- com/load0.asm
+++ com/load0.asm
@@ -11,17 +11,62 @@ L0:
         mov     dx,LPT_CONTROL
         mov     al,00000100b
         out     dx,al
-
+%if 0
         in      al,dx
         or      al,30h
         mov     dl,al
         mov     ah,2
         int     21h
         jmp     short L0
+%endif
 
+        mov     di,0200h
+        mov     cx,256
+readloop:
+; Wait for valid high
+        dec     dx      ;LPT_STATUS
+waitvalid1:
+        in      al,dx
+        test    al,8
+        jz      short waitvalid1
+        and     al,0f0h ; preserve in bl
+        xchg    ax,bx
+
+; Signal READY by raising DATA.0
+        dec     dx      ; LPT_DATA
+        mov     al,1
+        out     dx,al
+
+; Capture data in bl
+        inc     dx
+        inc     dx      ; LPT_CONTROL
+        in      al,dx
+        and     al,00fh
+        or      bl,al
+
+; Wait for valid low
+        dec     dx      ; LPT_STATUS
+waitvalid0:
+        in      al,dx
+        test    al,8
+        jnz     short waitvalid0
+
+; Drop ready
+        dec     dx      ; LPT_DATA
+        mov     al,0
+        out     dx,al
+
+; Store bl
+        mov     al,bl
+        stosb
+        loop    readloop
+        int     3
+
+%if 0
         mov     ah,09
         mov     dx,message
         int     21h
         mov     ah,4ch
         int     21h
 message   db "Hello", 13,10,"$"
+%endif
--- com/unittest.fs
+++ com/unittest.fs
@@ -12,26 +12,54 @@ include numeric.fs
     <> throw
 ;
 
+: d123
+    d# 1 d# 2 d# 3
+;
+
 : main
-  d# 10 base !
-  banner
+    d# 10 base !
+    banner
 
-  d# 0 part
-  d# 1 d# 2 +           d# 3 T
+    d# 0 part
+    d# 1 d# 2 +             d# 3 T
 
-  d# 1 part
-  d# 5 d# 8 lshift      h# 0500 T
-  h# ffff d# 9 lshift   h# fe00 T
-  h# ffff d# 9 rshift   h# 007f T
+    d# 1 part
+    d# 5 d# 8 lshift        h# 0500 T
+    h# ffff d# 9 lshift     h# fe00 T
+    h# ffff d# 9 rshift     h# 007f T
 
-  d# 2 part
-  d# -1 2/              h# ffff T
-  d# 1 2/               h# 0000 T
-  h# 5555 2/            h# 2aaa T
+    d# 2 part
+    d# -1 2/                h# ffff T
+    d# 1 2/                 h# 0000 T
+    h# 5555 2/              h# 2aaa T
 
-  depth d# 0 T
-  cr
-  [char] O emit
-  [char] K emit
-  quit
+    d# 3 part
+    d123                    d# 3 T d# 2 T d# 1 T
+    d123 swap               d# 2 T d# 3 T d# 1 T
+    d123 rot                d# 1 T d# 3 T d# 2 T
+    d123 -rot               d# 2 T d# 1 T d# 3 T
+    d123 tuck               d# 3 T d# 2 T d# 3 T d# 1 T
+    d123 xor xor            d# 0 T
+
+    d# 4 part
+    d# 8 d# 0
+    begin
+        2dup <>
+    while
+        1+
+    repeat                  d# 8 T d# 8 T
+
+    d# 5 part
+    d# 100 d# 101 max       d# 101 T
+    d# 100 d# 101 min       d# 100 T
+
+    d# 6 part
+    d# 1000.                d# 0 T d# 1000 T
+    d# -1000.               h# FFFF T h# FC18 T
+
+    depth d# 0 T
+    cr
+    [char] O emit
+    [char] K emit
+    quit
 ;
